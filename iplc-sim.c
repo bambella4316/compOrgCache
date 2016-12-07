9f1e0c88dc809e8e7fb60dfc05392952a3a9b2a1
bambella4316 <bmaitland97@gmail.com>
2016-12-07 11:53:53 -0500

    Added LW and SW
    Process Pipeline LW and Process Pipeline SW

--- iplc-sim.c
+++ iplc-sim.c
@@ -357,12 +357,27 @@ void iplc_sim_process_pipeline_rtype(char *instruction, int dest_reg, int reg1, 
 
 void iplc_sim_process_pipeline_lw(int dest_reg, int base_reg, unsigned int data_address)
 {
-    /* You must implement this function */
+    iplc_sim_push_pipeline_stage();
+
+    pipeline[FETCH].itype = LW;
+    pipeline[FETCH].instruction_address = instruction_address;
+
+    pipeline[FETCH].stage.lw.dest_reg = dest_reg;
+    pipeline[FETCH].stage.lw.base_reg = base_reg;
+    pipeline[FETCH].stage.lw.data_address = data_address;
+
 }
 
 void iplc_sim_process_pipeline_sw(int src_reg, int base_reg, unsigned int data_address)
 {
-    /* You must implement this function */
+    iplc_sim_push_pipeline_stage();
+
+    pipeline[FETCH].itype = SW;
+    pipeline[FETCH].instruction_address = instruction_address;
+
+    pipeline[FETCH].stage.sw.src_reg = src_reg;
+    pipeline[FETCH].stage.sw.base_reg = base_reg;
+    pipeline[FETCH].stage.sw.data_address = data_address;
 }
 
 void iplc_sim_process_pipeline_branch(int reg1, int reg2)
